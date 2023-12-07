74ae69ab863acf430163ea68ab6d3a747876b615
chnadru123 <chandrashekhar.13ec012@pace.edu.in>
2023-12-07 07:02:47 +0000

    HSE_App

---
+++ hse/hse/doctype/acm_verified_assessed_by__aen_qs/acm_verified_assessed_by__aen_qs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Seyfert and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ACM Verified Assessed By  AEN QS', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ hse/hse/doctype/confined_space_rescue_plan/confined_space_rescue_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Seyfert and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Confined Space Rescue Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
